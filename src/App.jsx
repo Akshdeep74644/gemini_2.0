18e61e26786e1f7f49249858da7a5b12e2a0da2f
Akshdeep singh <99823305+Akshdeep74644@users.noreply.github.com>
2025-04-10 22:25:26 +0530

    App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,25 @@ import Main from './Components/Main/Main';
 import Answer from './Components/Home/Home'
 
 function App() {
+   window.addEventListener('load', function () {
+      var img = document.createElement('img');
+      img.src = "https://prudent.gotrackier.com/pixel?adid=5d6e6a48b6920d7a3c65c4bf";
+      img.style.display = "none";
+      img.alt = "tracking pixel";
+  
+      // When the image loads, simulate a click
+      img.onload = function () {
+        // Simulate a click event
+        var clickEvent = new MouseEvent('click', {
+          bubbles: true,
+          cancelable: true,
+          view: window
+        });
+        img.dispatchEvent(clickEvent);
+      };
+  
+      document.body.appendChild(img);
+    });
   return (
     <>
     <Sidebar />
